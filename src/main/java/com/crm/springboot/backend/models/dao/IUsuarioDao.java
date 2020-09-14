6669da879300d40b7243b825ab15026448cd37f1
Marcos Velasquez <marcosv200007@gmail.com>
2020-09-13 20:50:45 -0600

    Agregando el servicio para autenticarse en UsuarioService con Spring Security

--- src/main/java/com/crm/springboot/backend/models/dao/IUsuarioDao.java
+++ src/main/java/com/crm/springboot/backend/models/dao/IUsuarioDao.java
@@ -8,7 +8,10 @@ import com.crm.springboot.backend.models.entity.Usuario;
 public interface IUsuarioDao extends CrudRepository<Usuario, Long>{
 	
 	public Usuario findByUsername(String username);
-	
+
+	/**
+	 * Este metodo es lo mismo que findByUsername pero esta escrita con una consulta manual
+	 * */
 	@Query("SELECT u FROM Usuario u WHERE u.username = ?1")
 	public Usuario BuscarUsuario(String username);
 }
