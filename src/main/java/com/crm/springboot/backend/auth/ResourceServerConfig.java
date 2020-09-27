7530324b7a1258e72dfb430c0f1f3ac02a3c4b92
Marcos Velásquez <marcosv200007@gmail.com>
2020-09-27 17:39:54 -0600

    Cambios para autorizar visualizacion de imagenes

--- src/main/java/com/crm/springboot/backend/auth/ResourceServerConfig.java
+++ src/main/java/com/crm/springboot/backend/auth/ResourceServerConfig.java
@@ -22,7 +22,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/clientes", "/api/clientes/page/**", "uploads/img/**", "/images/**").permitAll()
+		http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/clientes", "/api/clientes/page/**", "/api/uploads/img/**", "/images/**").permitAll()
 		/*.antMatchers(HttpMethod.GET, "/api/clientes/{id}").hasAnyRole("USER", "ADMIN")
 		.antMatchers(HttpMethod.POST, "/api/clientes/upload").hasAnyRole("USER", "ADMIN")
 		.antMatchers(HttpMethod.POST, "/api/clientes").hasRole("ADMIN")
