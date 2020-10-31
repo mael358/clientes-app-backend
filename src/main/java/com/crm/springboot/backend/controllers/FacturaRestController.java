62fd41d1a63dad8187a3fc4f634bcdb886781503
Marcos Velasquez <marcosv200007@gmail.com>
2020-10-30 21:02:40 -0600

    Implementando seguridad a las rutas de Facturas
    Se regresaron los roles a como estaban y se agrego la seguridad a
    ciertos metodos de facturas, como el de eliminar, solo para
    administradores.

--- src/main/java/com/crm/springboot/backend/controllers/FacturaRestController.java
+++ src/main/java/com/crm/springboot/backend/controllers/FacturaRestController.java
@@ -5,6 +5,7 @@ import com.crm.springboot.backend.models.entity.Producto;
 import com.crm.springboot.backend.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,23 +18,27 @@ public class FacturaRestController {
     @Autowired
     private IClienteService clienteService;
 
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
     @GetMapping("/facturas/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Factura show(@PathVariable Long id){
         return clienteService.findFacturaById(id);
     }
 
+    @Secured({"ROLE_ADMIN"})
     @DeleteMapping("/facturas/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id){
         clienteService.deleteFacturaById(id);
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
     @GetMapping("/facturas/filtrar-productos/{term}")
     public List<Producto> filtrarProductos(@PathVariable String term){
         return clienteService.findProductoByNombre(term);
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
     @PostMapping("/facturas")
     @ResponseStatus(HttpStatus.CREATED)
     public Factura crear(@RequestBody Factura factura){
