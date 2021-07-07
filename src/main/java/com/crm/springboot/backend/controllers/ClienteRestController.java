aa79c67a863f2b19e0e959a20a3bb807f6758dbe
Marcos Velásquez <mevh@elpilar.gt>
2021-07-06 18:12:38 -0600

    Commit

--- src/main/java/com/crm/springboot/backend/controllers/ClienteRestController.java
+++ src/main/java/com/crm/springboot/backend/controllers/ClienteRestController.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import javax.validation.Valid;
+import javax.validation.Valid; 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
